291be5dbe860d62e447106272c50e4233cac35a4
Asfandyar <118163641+Asfandyar-creator@users.noreply.github.com>
2023-08-29 01:10:28 +0500

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,114 @@
+import matplotlib.pyplot as plt
+import streamlit as st
+import seaborn as sns
+import preprocessor, helper
+
+
+st.sidebar.title('Whatsapp Chat Analyzer')
+
+uploaded_file = st.sidebar.file_uploader('Choose a File')
+if uploaded_file is not None:
+    bytes_data = uploaded_file.getvalue()
+    data = bytes_data.decode('utf-8')
+    df = preprocessor.preprocess(data)
+
+    # Fetch unique users
+    user_list = df['user'].unique().tolist()
+    user_list.remove('group_notification')
+    user_list.sort()
+    user_list.insert(0, 'Overall')
+    selected_user = st.sidebar.selectbox('Show Analysis w.r.t ', user_list)
+
+    if st.sidebar.button('Show Analysis'):
+        num_messages, words, num_media_messages, num_links = helper.fetch_stats(selected_user, df)
+        st.title('Top Statistics')
+        col1, col2, col3, col4 = st.columns(4)
+
+        with col1:
+            st.text('Total Messages')
+            st.text(num_messages)
+        with col2:
+            st.text('Number of Words')
+            st.text(words)
+        with col3:
+            st.text('Media Shared')
+            st.text(num_media_messages)
+        with col4:
+            st.text('Total Links Shared')
+            st.text(num_links)
+        
+        # Monthly Timeline
+        st.title('Monthly Timeline')
+        timeline = helper.monthly_timeline(selected_user, df)
+        fig, ax = plt.subplots()
+        ax.plot(timeline['time'], timeline['message'], color='green')
+        plt.xticks(rotation='vertical')
+        st.pyplot(fig)
+
+        # Daily Timeline
+        st.title('Daily Timeline')
+        timeline = helper.daily_timeline(selected_user, df)
+        fig, ax = plt.subplots()
+        ax.plot(timeline['only_date'], timeline['message'], color='green')
+        plt.xticks(rotation='vertical')
+        st.pyplot(fig)
+
+        # Activity Map
+        st.title('Activity Map')
+        col1, col2 = st.columns(2)
+
+        with col1:
+            st.header('Most Busy Day')
+            busy_day = helper.week_activity_map(selected_user, df)
+            fig, ax = plt.subplots()
+            ax.bar(busy_day.index, busy_day.values)
+            plt.xticks(rotation='vertical')
+            st.pyplot(fig)
+        with col2:
+            st.header('Most Busy Month')
+            busy_month = helper.month_activity_map(selected_user, df)
+            fig, ax = plt.subplots()
+            ax.bar(busy_month.index, busy_month.values, color='orange')
+            plt.xticks(rotation='vertical')
+            st.pyplot(fig)
+
+        st.title("Weekly Activity Map")
+        user_heatmap = helper.activity_heatmap(selected_user,df)
+        fig,ax = plt.subplots()
+        ax = sns.heatmap(user_heatmap)
+        st.pyplot(fig)
+
+        # Fetching the busiest users in the group (group level)
+        if selected_user == 'Overall':
+            st.text('Most Busy Users')
+            x, new_df = helper.most_busy_users(df)
+            fig, ax = plt.subplots()
+            
+            col1, col2 = st.columns(2)
+            with col1:
+                ax.bar(x.index, x.values)
+                plt.xticks(rotation='vertical')
+                st.pyplot(fig)
+            with col2:
+                st.dataframe(new_df)
+            
+        
+        # WordCloud
+        st.title('Wordcloud')
+        df_wc = helper.create_wordcloud(selected_user, df)
+        fig, ax = plt.subplots()
+        ax.imshow(df_wc)
+        st.pyplot(fig)
+
+        # Most common words
+        most_common_df = helper.most_common_words(selected_user, df)
+        fig, ax = plt.subplots()
+
+        ax.barh(most_common_df['Word'], most_common_df['Frequency'])
+        plt.xticks(rotation='vertical')
+        st.title('Most Common Words')
+        st.pyplot(fig)
+
+        # Emojis analysis
+        emojis_df = helper.emojis_helper(selected_user, df)
+        st.dataframe(emojis_df)
